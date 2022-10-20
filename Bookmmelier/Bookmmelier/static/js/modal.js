5fea7d12dde9cb73188c4bb6b2a3d97a07c3740d
LeeSeungHyun9661 <dltmdgus9661@gmail.com>
2022-10-20 17:01:36 +0900

    STATIC 적용

---
+++ Bookmmelier/Bookmmelier/static/js/modal.js
@@ -0,0 +1,18 @@
+function popOpen() {
+
+    var modalPop = document.querySelector('.modal-wrap');
+    var modalBg = document.querySelector('.modal-bg');
+
+    modalPop.style.display = 'block';
+    modalBg.style.display = 'block';
+
+}
+
+function popClose() {
+    var modalPop = document.querySelector('.modal-wrap');
+    var modalBg = document.querySelector('.modal-bg');
+
+    modalPop.style.display = 'none';
+    modalBg.style.display = 'none';
+
+}
